9ffed4d4ca3828e56e6035c4948cba932de7ced6
chris <montymontgomeri@gmail.com>
2020-02-11 04:25:58 -0500

    First whack at how the board will work. Still haven't figured out graphics

--- src/main/java/Cell.java
+++ src/main/java/Cell.java
@@ -1,30 +1,31 @@
-import java.awt.Color;
-import javax.swing.JFrame;
-import java.awt.Font;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextArea;
-import javax.swing.Timer;
 
 public class Cell {
 	
-	boolean isAlive = false;
+	private boolean isAlive = false;
 	
+	public boolean askIsAlive(int livingCells) {
+		if(livingCells==2||livingCells==3) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 	
+	public void changeAlive(boolean isAliveOrIsDead) {
+		
+		boolean changeStatus = false;
+		
+		if(isAliveOrIsDead==true) {
+			changeStatus = false;
+		} else if (isAliveOrIsDead==false) {
+			changeStatus = true;
+		}
+		
+		this.isAlive = changeStatus;
+	}
 
 	public boolean isAlive() {
 		return isAlive;
-	}
-
-	public void setAlive(boolean isAlive) {
-		this.isAlive = isAlive;
 	}
 
 }
--- src/main/java/GameOfLife.java
+++ src/main/java/GameOfLife.java
@@ -6,6 +6,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -14,6 +17,11 @@ import javax.swing.JTextArea;
 import javax.swing.Timer;
 
 public class GameOfLife {
+	
+	static int[][] gameBoard;
+	static int numberOfCells = 4000;
+	static int numberOfRows = 51;
+	static int numberOfColumns = 81;
 	
 	 public static void main(String[] args) {
 		new GameOfLife(); 
@@ -37,7 +45,30 @@ public class GameOfLife {
 		board.setLayout(new GridLayout(50,80));
 		window.add(board);
 		
-		JButton cell = new JButton();
+		JButton cell1 = new JButton();
+		cell1.setBackground(Color.WHITE);
+		
+		
+	}
+	
+	public static void setBoard(boolean isAlive) { 
+		
+		Cell cell = new Cell();
+		
+		for(int i = 1; i < numberOfRows; i++) {
+			for(int j = 1; j < numberOfColumns; j++) {
+				
+				int liveCells = gameBoard[i-1][j-1] + gameBoard[i-1][j] + gameBoard[i-1][j+1] + gameBoard[i][j-1] + gameBoard[i][j+1] + gameBoard[i+1][j-1] + gameBoard[i+1][j] + gameBoard[i+1][j+1];
+				
+				if(cell.askIsAlive(liveCells)) {					
+					gameBoard[i][j] = 1;
+				} else {
+					gameBoard[i][j] = 0;
+				}
+				
+				
+			}
+		}
 		
 	}
 }
