375e71fff254e5adade2badba57f75079c2a03fa
chris <montymontgomeri@gmail.com>
2020-02-11 02:36:21 -0500

    Second commit. Setting up repository & figuring out graphical display

--- src/main/java/Cell.java
+++ src/main/java/Cell.java
@@ -1,4 +1,30 @@
+import java.awt.Color;
+import javax.swing.JFrame;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.Timer;
 
 public class Cell {
+	
+	boolean isAlive = false;
+	
+	
+
+	public boolean isAlive() {
+		return isAlive;
+	}
+
+	public void setAlive(boolean isAlive) {
+		this.isAlive = isAlive;
+	}
 
 }
